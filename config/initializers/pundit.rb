fc5bf44a8bf451f4e6c9f126aac33932410e4231
BSorbus <BSorbus@gmail.com>
2020-06-01 00:28:23 +0200

    Pundit check request.referrer.host for action from CSU

--- config/initializers/pundit.rb
+++ config/initializers/pundit.rb
@@ -19,12 +19,15 @@ module PunditHelper
       render status: :forbidden, json: {error: "#{mess}" }
     elsif request.format == 'html'
       flash[:error] = "#{mess}"
-      puts '------------pundit.rb user_not_authorized(exception)--------------------'
-      puts request.referrer
-      puts '------------------------------------------------------------------------'
+      referrer_or_root = request.referrer
+      if referrer_or_root.present? 
+        referrer_or_root = root_path unless referrer_or_root.host == "#{Rails.application.secrets[:domain_name]}"
+      else
+        referrer_or_root = root_path
+      end
       # redirect_to(:back) is being deprecated in Rails 5
       # redirect_to(request.referrer || root_path)
-      redirect_back( fallback_location: request.referrer || root_path, allow_other_host: false )
+      redirect_back( fallback_location: referrer_or_root, allow_other_host: false )
     else
       head :forbidden
     end
